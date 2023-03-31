c9104b025699ed8f4422ee6210170211f196df46
Akenzhebulatov <akenzhebulatov@one.kz>
2023-04-01 05:55:17 +0600

    inital commit

---
+++ js/generator.js
@@ -0,0 +1,12 @@
+function generatePassword(length) {
+  const charset =
+    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"; // A = 62
+  let retVal = "";
+  for (var i = 0, n = charset.length; i < length; ++i) {
+    retVal += charset.charAt(Math.floor(Math.random() * n));
+  }
+  return retVal;
+}
+
+const length = 6; // L = 6
+console.log(generatePassword(length));
